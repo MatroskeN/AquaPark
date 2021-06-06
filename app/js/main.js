d8c5d1380495bee2b25775c13d9edc377c303e6d
Matrosken <piranya1399@gmail.com>
2021-06-06 23:51:06 +0400

    ауа

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function (){
-    
+
+    var swiper = new Swiper('.services_container', {
+        slidesPerView: 'auto',
+        spaceBetween: '29',
+        navigation: {
+            nextEl: '.services_next',
+            prevEl: '.services_prev',
+          },
+      });
+      var swiper1 = new Swiper('.kid_container', {
+        slidesPerView: '1',
+        navigation: {
+            nextEl: '.kid_next',
+            prevEl: '.kid_prev',
+          },
+      });
     
 })
 
