432d709c254f9a83bb735f59e85ac54f940623bc
MatroskeN <44238649+MatroskeN@users.noreply.github.com>
2021-06-07 16:10:40 +0400

    ауа
    уауа

--- app/js/main.js
+++ app/js/main.js
@@ -2,19 +2,24 @@ $(document).ready(function (){
 
     var swiper = new Swiper('.services_container', {
         slidesPerView: 'auto',
-        spaceBetween: '29',
+        spaceBetween: 29,
         navigation: {
             nextEl: '.services_next',
             prevEl: '.services_prev',
           },
       });
       var swiper1 = new Swiper('.kid_container', {
-        slidesPerView: '1',
+        slidesPerView: 1,
+        centeredSlides: true,
+        spaceBetween: 29,
         navigation: {
             nextEl: '.kid_next',
             prevEl: '.kid_prev',
           },
       });
-    
+     
+      // let timerId = setInterval(() => 
+      // document.getElementsByClassName('footer_backvawe').style.backgroundPosition
+      // , 2000);
 })
 
